9ca25ecc3f3b6cdccd1cb18b975bb2bce35524fa
minseong1 <alstjd2627@naver.com>
2024-10-08 21:15:33 +0900

    Initial commit

---
+++ src/main/java/hongik/cartoonforblindbackend/CartoonForBlindBackendApplication.java
@@ -0,0 +1,13 @@
+package hongik.cartoonforblindbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CartoonForBlindBackendApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CartoonForBlindBackendApplication.class, args);
+  }
+
+}
---
+++ src/test/java/hongik/cartoonforblindbackend/CartoonForBlindBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package hongik.cartoonforblindbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CartoonForBlindBackendApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
