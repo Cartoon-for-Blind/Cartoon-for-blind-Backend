78afc03ef45ebbf73facdabc58335d582bfc62f4
minseong1 <alstjd2627@naver.com>
2024-11-07 17:37:11 +0900

    chore: cors url 추가(vercel)

--- src/main/java/hongik/cartoonforblindbackend/global/config/WebConfig.java
+++ src/main/java/hongik/cartoonforblindbackend/global/config/WebConfig.java
@@ -11,11 +11,11 @@ public class WebConfig implements WebMvcConfigurer {
   @Override
   public void addCorsMappings(CorsRegistry registry) {
     registry.addMapping("/**")
-        .allowedOrigins("http://localhost:3000") // 허용할 출처
+        .allowedOrigins("http://localhost:3000", "https://cartoon-for-blind-frontend.vercel.app")
         .allowedHeaders("*")
         .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE") // 허용할 HTTP method
         .allowCredentials(true) // 쿠키 인증 요청 허용
-        .exposedHeaders("*")
+        .exposedHeaders("ngrok-skip-browser-warning")
         .maxAge(3000); // 원하는 시간만큼 pre-flight 리퀘스트를 캐싱
   }
 }
